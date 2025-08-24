6c8e015d4fa558624d6443aa20979432aa168b73
febinalex <69387923+febinalex@users.noreply.github.com>
2025-08-24 20:23:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/projects/',   // <= important
+  base: '/projects/',          
   plugins: [react()],
+  build: { outDir: 'dist' }    /
 })
